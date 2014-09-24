f997ed3de1c76020c576b8bb899b0326be8225d4
suakig <suakig@gmail.com>
2014-09-24 23:34:22 +0900

    first setting

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -7,15 +7,15 @@ class HelloWorld : public cocos2d::Layer
 {
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    //HelloWorldクラスのシーンを作成する。
     static cocos2d::Scene* createScene();
 
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual bool init();  
-    
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
+    //初期か処理を行う
+    virtual bool init();
     
     // implement the "static create()" method manually
+    //create関数作成マクロ
     CREATE_FUNC(HelloWorld);
 };
 
