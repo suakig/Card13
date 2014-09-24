7fbc82ea599430e915114d3500f6d4a5ff1b7049
suakig <suakig@gmail.com>
2014-09-25 00:13:01 +0900

    show card

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -32,5 +32,77 @@ bool HelloWorld::init()
         return false;
     }
     
+    initGame();
+    
     return true;
+}
+
+void HelloWorld::initCards(){
+    
+    _cards.clear();
+    
+    for(int type = 0; type < CARD_TYPE_NUM; type++)
+    {
+          for(int number = 0; number < CARD_NUM; number++)
+          {
+              Card card;
+              card.number = number;
+              card.type = (CardType)type;
+              
+              _cards.push_back(card);
+          }
+    }
+}
+
+Card HelloWorld::getCard()
+{
+    std::random_device rd;
+    std::mt19937 rand = std::mt19937(rd());
+    
+    int index = std::uniform_int_distribution<int>(0, (int)_cards.size() - 1)(rand);
+    
+    //カードの値を習得する
+    auto card = _cards[index];
+    
+    //一時カードから削除
+    _cards .erase(_cards.begin() + index);
+    
+    return card;
+}
+
+void HelloWorld::createCard(PosIndex posIndex)
+{
+    //新しいカードを作成する
+    auto card = Sprite::create("card_spades.png");
+    card->setPosition(CARD_1_POS_X + CARD_DISTANCE_X * posIndex.x,
+                      CARD_1_POS_Y + CARD_DISTANCE_Y * posIndex.y);
+    addChild(card, ZORDER_SHOW_CARD);
+}
+
+void HelloWorld::showInitCards()
+{
+    for(int tag = 1; tag <= 10; tag++)
+    {
+        auto card = getChildByTag(tag);
+        if (card) {
+            card->removeFromParent();
+        }
+    }
+    
+    for (int x = 0; x < 5; x++) {
+        for (int y = 0; y < 2; y++) {
+            PosIndex posIndex;
+            posIndex.x = x;
+            posIndex.y = y;
+            
+            createCard(posIndex);
+        }
+    }
+}
+
+void HelloWorld::initGame()
+{
+    initCards();
+    
+    showInitCards();
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -2,9 +2,54 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include <random>
+
+#define CARD_NUM 13//１種類あたりのカードの枚数
+#define CARD_TYPE_NUM 4//カードの種類
+
+#define CARD_1_POS_X 200
+#define CARD_1_POS_Y 320
+#define CARD_DISTANCE_X 140
+#define CARD_DISTANCE_Y 200
+
+#define ZORDER_SHOW_CARD 1//表示しているカードのZオーダー
+
+USING_NS_CC;
+
+//カードの種類
+enum CardType
+{
+    Clubs =0,
+    Diamonds,
+    Hearts,
+    Spades
+};
+
+//カード
+struct Card
+{
+    int number;
+    CardType type;
+};
+
+//カードを表示する位置
+struct PosIndex{
+    int x;
+    int y;
+};
+
 
 class HelloWorld : public cocos2d::Layer
 {
+protected:
+    std::vector<Card> _cards;//カード情報
+    
+    void initCards();//カードを初期化する
+    Card getCard();//カードを習得する
+    void createCard(PosIndex posIndex);//カードを生成
+    void showInitCards();//ゲーム開始時にカードを10枚表示する。
+    void initGame();//ゲームを初期化する。
+    
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     //HelloWorldクラスのシーンを作成する。
